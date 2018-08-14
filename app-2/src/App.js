dce5fcec0cc6752de5bcdb6b0c4d48734ac5927c
Jay Yu <jay.nam28@gmail.com>
2018-08-13 21:26:29 -0500

    complete

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      foods: ['Rice', 'Kimchi', 'Steak', 'Chicken', 'Fish']
+    }
+  }
+
+
+
   render() {
+    let display = this.state.foods.map(el => {
+      return <h2>{el}</h2>
+    })
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {display}
+
       </div>
     );
   }
--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,18 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      foods: [
+        "Rice",
+        "Kimchi",
+        "Steak",
+        "Chicken",
+        "Fish",
+        "Noodles",
+        "Pizza",
+        "Eggs"
+      ],
+      userInput: ""
+    };
+  }
+
+  handleChange(val) {
+    this.setState({ userInput: val });
+  }
+
   render() {
+    /*let display = this.state.foods.map(el => {
+      return <h2>{el}</h2>
+    })*/
+    let display = this.state.foods
+      .filter((el, index) => {
+        return el.toLowerCase().includes(this.state.userInput.toLowerCase());
+      })
+      .map(food => <div>{food}</div>);
+
+    // return <h4>{this.state.foods[index]}</h4>;
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange={e => this.handleChange(e.target.value)} />
+        {display}
       </div>
     );
   }
